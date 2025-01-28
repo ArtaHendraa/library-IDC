aafe86a05eb039ff8a8144477757ee4b6d423ea9
ArtaHendraa <artahenda@gmail.com>
2025-01-29 00:49:31 +0800

    chore: create API controller with index and store methods

--- app/Http/Controllers/Api/BooksController.php
+++ app/Http/Controllers/Api/BooksController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Books;
+use Exception;
 
 class BooksController extends Controller
 {
@@ -12,7 +14,29 @@ class BooksController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $books = Books::paginate(25);
+            // $total = $books->count();
+            return response()->json([
+              "status_code" => 200,
+              "total_data" => $books -> total(),
+              "data_per_page" => $books -> perPage(), 
+              "last_page" => $books -> lastPage(),
+              "success" => true,
+              "message" => "Success Fetching Book Data",
+              "next_page" => $books -> nextPageUrl(), 
+              "current_page" => $books -> currentPage(), 
+              "prev_page" => $books->previousPageUrl(),
+              "data" => $books -> items()
+            ], 200);
+        } catch (Exception $error) {
+            return response()->json([
+              "status_code" => 500,
+              "data" => null,
+              "success" => false,
+              "message" => "$error"
+            ], 500);
+        }
     }
 
     /**
@@ -20,7 +44,47 @@ class BooksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+              "title" => "required|string|min:5|max:255",
+              "description" => "required|string|min:50",
+              "author" => "required|string|min:4|max:255",
+            ],[
+              "title.required" => "Title Cannot Be Empty",
+              "title.string" => "Title Must Be A String",
+              "title.min" => "Title Must Have At Least 5 Characters",
+              "title.max" => "Title Must Have A Maksimum Of 255 Characters",
+
+              "description.required" => "Description Cannot Be Empty",
+              "description.string" => "Description Must Be A String",
+              "description.min" => "Description Must Have At Least 50 Characters",
+
+              "author.required" => "Author Cannot Be Empty",
+              "author.string" => "Author Must Be A String",
+              "author.min" => "Author Must Have At Least 4 Characters",
+              "author.max" => "Author Must Have A Maksimum Of 255 Characters",
+            ]);
+
+            Books::create([
+              "title" => $request->title,
+              "description" => $request->description,
+              "author" => $request->author,
+            ]);
+
+            return response()->json([
+              "code" => 201,
+              "success" => true,
+              "message" => "Berhasil Menambahkan Data Post!"
+            ], 201);
+
+        } catch (Exception $error) {
+            return response()->json([
+              "status_code" => 422,
+              "data" => null,
+              "success" => false,
+              "message" => "$error"
+            ], 422);
+        }
     }
 
     /**
